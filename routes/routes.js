74f2c4be1574c16e4ad84f65c1cb572e8d9344af
Jason L <jluboff@gmail.com>
2017-08-01 16:34:31 -0700

    Used backend to handle user going/not going

--- routes/routes.js
+++ routes/routes.js
@@ -14,10 +14,9 @@ module.exports = (app, passport, db) => {
       if(req.user !== undefined){
       user = `${req.user._json.first_name} ${req.user._json.last_name}`;
       avatar = req.user._json.picture.data.url;
-    };
-    location = req.session.location !== undefined ? req.session.location : undefined;
-    console.log(req.session);
-    res.render('index.hbs', {user, avatar, location});
+      };
+      location = req.session.location !== undefined ? req.session.location : undefined;
+      res.render('index.hbs', {user, avatar, location});
     });
 
   app.route('/auth/facebook/callback')
@@ -40,15 +39,13 @@ module.exports = (app, passport, db) => {
       let city = req.session.location.toLowerCase();
       db.collection('location').find({city}).toArray((err, doc) => {
         if (err) throw err;
-        console.log(`Found...: ${doc}`);
+
         if(doc.length){
-        console.log(`Check`);
         return res.json(doc);
       } else {
         yelp.accessToken('dNlo7ZOX2VzRimvxUmEiXA', 'jhrkuiwW1U4kfgSarByzubXNTOecaWDTorHJv3GPKle4rYA60pmamqd55uXGQhT7')
                               .then(response => {
-                                //console.log(response.jsonBody.access_token);
-  const client = yelp.client(response.jsonBody.access_token);
+                                const client = yelp.client(response.jsonBody.access_token);
 
                                 client.search({
                                   term: 'Bar',
@@ -63,12 +60,12 @@ module.exports = (app, passport, db) => {
                                     business['image'] = biz.image_url;
                                     business['rating'] = biz.rating;
                                     business['count'] = 0;
+                                    business['user'] = [];
                                     businesses.push(business);
                                   })
+
                                   db.collection('location').insertMany(businesses, (err, doc) => {
                                     if(err) throw err;
-
-                                  console.log(`Had to insert..: ${JSON.stringify(doc)}`);
 
                               res.json(doc.ops);
                                     });
@@ -80,32 +77,33 @@ module.exports = (app, passport, db) => {
     })
 
   app.route('/going/:city/:locationName')
-  /*  .get((req, res) => {
-      let city = req.params.city.toLowerCase();
-      console.log('Request recieved: ' + city + " " + req.params.locationName);
-      db.collection('location').updateOne({city, bar: req.params.locationName},
-            {$inc:{ count : 1}},
-            {upsert: true });
-
-    return;
-  }) */
     .post(isLogged, (req, res) => {
       let city = req.params.city.toLowerCase();
-    /*  db.collection('location').updateOne({city: city, name: req.params.locationName},
-            {$inc:{ count : 1}},
-            {upsert: true });
-      db.collection('location').findOne({city: city, name: req.params.locationName}, (err, doc) => {
-        if(err) throw err;
+      let user = `${req.user._json.first_name} ${req.user._json.last_name}`;
+      let avatar = req.user._json.picture.data.url;
 
-        res.json(doc);
-      }) */
-        console.log("City: " + city + " Name: " + req.params.locationName)
-      db.collection('location').findOneAndUpdate({city: city, name: req.params.locationName},
-              {$inc:{ count : 1}},
-              {upsert: true, returnOriginal: false }, (err, doc) => {
-                if(err) throw err;
-                  console.log(doc);
-                res.json(doc.value);
-              })
+        console.log("City: " + city + " Name: " + req.params.locationName + " User: " + req.user._json.first_name + ' ' + req.user._json.last_name);
+//db.location.find({city: 'ventura', user: {$elemMatch: {$elemMatch: {$in: ['Jason Luboff']}}}})
+      db.collection('location').findOne({city: city, name: req.params.locationName, user: {$elemMatch: {$elemMatch: {$in: ['Jason Luboff']}}}}, (err, doc) => {
+        if(err) throw err;
+        if(doc){
+          db.collection('location').findOneAndUpdate({city: city, name: req.params.locationName},
+                  {$inc:{ count : -1}, $pull: {user: { $in: [user] } }},
+                  {upsert: true, returnOriginal: false }, (err, doc) => {
+                    if(err) throw err;
+                      console.log(doc);
+                      return res.json(doc.value);
+                  })
+        } else {
+          db.collection('location').findOneAndUpdate({city: city, name: req.params.locationName},
+                  {$inc:{ count : 1}, $addToSet: {user: [user, avatar]}},
+                  {upsert: true, returnOriginal: false }, (err, doc) => {
+                    if(err) throw err;
+                      console.log(doc);
+                      return res.json(doc.value);
+                  })
+        };
+      })
+
     })
 };
